c138a99d94032fbf394ae82142af5a36a91a45a1
scorl <scorl@LAPTOP-HP-STE.homenet.telecomitalia.it>
2019-08-28 19:41:13 +0200

    changes scene works

--- prova_fx/src/application/Sample2Controller.java
+++ prova_fx/src/application/Sample2Controller.java
@@ -1,20 +1,37 @@
 package application;
 
 import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
 
-public class Sample2Controller {
+public class Sample2Controller implements Initializable {
 
 	@FXML
 	BorderPane scene2;
 	
 	@FXML
 	void doClick2(ActionEvent event) throws IOException {
-		BorderPane pane = (BorderPane)FXMLLoader.load(getClass().getResource("Sample1.fxml"));
-		scene2.getChildren().setAll(pane);
+		Parent parent =FXMLLoader.load(getClass().getResource("Sample.fxml"));
+    	Scene scene = new Scene(parent);
+    	Stage window = (Stage)((Node)event.getSource()).getScene().getWindow();
+    	
+    	window.setScene(scene);
+    	window.show();
+	}
+
+	@Override
+	public void initialize(URL arg0, ResourceBundle arg1) {
+		// TODO Auto-generated method stub
+		
 	}
 }
--- prova_fx/src/application/SampleController.java
+++ prova_fx/src/application/SampleController.java
@@ -2,12 +2,16 @@ package application;
 
 import java.io.IOException;
 import java.net.URL;
+import java.util.LinkedList;
 import java.util.ResourceBundle;
 
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
@@ -15,18 +19,31 @@ import javafx.stage.Stage;
 public class SampleController implements Initializable{
 	
 	@FXML
-	BorderPane scene1;
+	BorderPane scene;
 	
 	@FXML
-    void doClick(ActionEvent event) throws IOException {
-		
-    	BorderPane pane = (BorderPane)FXMLLoader.load(getClass().getResource("Sample2.fxml"));
-//    	Scene scene = new Scene(pane);
-//    	scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
-//    	stage.setScene(scene);
-//    	stage.show();
-    	scene1.getChildren().setAll(pane);
-  
+	Stage stage;
+	
+	@FXML
+    public void doClick(ActionEvent event) throws IOException {
+		ObservableList list = scene.getChildren();
+    	System.out.println(list.toString());
+//    	BorderPane pane = (BorderPane)FXMLLoader.load(getClass().getResource("Sample2.fxml"));
+    	Parent parent =FXMLLoader.load(getClass().getResource("Sample2.fxml"));
+    	Scene scene2 = new Scene(parent);
+    	Stage window = (Stage)((Node)event.getSource()).getScene().getWindow();
+    	
+    	window.setScene(scene2);
+    	window.show();
+    	
+    	
+//    	scene.getChildren().setAll(pane);
+    	
+    	ObservableList list2 = scene.getChildren();
+    	System.out.println(list2.toString());
+    	
+    	
+    	
 	}
 
 	@Override
