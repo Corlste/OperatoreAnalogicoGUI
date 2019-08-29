3ba7160d2df72f89a2d37b9bab6a44b380991d8f
scorl <scorl@10.102.1.144>
2019-08-29 16:55:17 +0200

    prime due scene

--- Operatore_BOT_GUI/src/Operatore_BOT_GUI/controller/StartController.java
+++ Operatore_BOT_GUI/src/Operatore_BOT_GUI/controller/StartController.java
@@ -1,5 +1,27 @@
 package Operatore_BOT_GUI.controller;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.event.ActionEvent;
+
+
 public class StartController {
-	
+
+    @FXML
+    private ComboBox<?> cmbAzienda;
+
+    @FXML
+    private Button btnCerca;
+
+    @FXML
+    void doBtnCerca(ActionEvent event) {
+
+    }
+
+    @FXML
+    void doCmbAzienda(ActionEvent event) {
+
+    }
+
 }
